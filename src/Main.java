13785c036365c1112d14d7e574c6830e32eb59e7
Arthur Gonçalves de Oliveira <oliveraarthurrtx@gmail.com>
2026-04-30 17:58:50 -0300

    comeco da atividade

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JOGO DE ADIVINHACAO");
+        System.out.println("tente adivinhar adivinhaar o numero entre 0 e 100");
+        System.out.println("o maximo de tentativas e 5");
+
+        Scanner sc = new Scanner(System.in);
+
+            new Random().nextInt();
+            int numAleatorio = sc.nextInt();
+            int tentativas = 0;
+
+        while (tentativas <5);
+
+        }
+
+    }
+}
