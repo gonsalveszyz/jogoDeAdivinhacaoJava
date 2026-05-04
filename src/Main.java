3643128488ee0e72030ad992584ba390f3317bad
Arthur Gonçalves de Oliveira <oliveraarthurrtx@gmail.com>
2026-05-04 14:30:34 -0300

    add code lines

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
+
 import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JOGO DE ADIVINHACAO");
-        System.out.println("tente adivinhar adivinhaar o numero entre 0 e 100");
-        System.out.println("o maximo de tentativas e 5");
-
         Scanner sc = new Scanner(System.in);
+        System.out.println("Jogo da Adivinhacao");
+        System.out.println("Adivinhe o numero secreto");
 
-            new Random().nextInt();
-            int numAleatorio = sc.nextInt();
-            int tentativas = 0;
+        int numAleatorio =  new Random().nextInt(101);
+        int tentativas = 0;
+        int acertos = 1;
 
-        while (tentativas <5);
+        while (tentativas <5 ) {
+            System.out.println("Digite um palpite: ");
+            int palpite = sc.nextInt();
 
+            tentativas++;
+
+            if (palpite == numAleatorio) {
+                System.out.println("Voce acertou em " + tentativas + " tentativas.");
+                break;
+            } else if (palpite < numAleatorio) {
+                System.out.println("O numero e maior! " );
+            }else if (palpite > numAleatorio) {
+                System.out.println("O numero e menor! "  );
+
+            }else if (acertos >0) {
+                System.out.println( "Voce acertou ");
+            }
+        }if (numAleatorio != numAleatorio) {
+            System.out.println("Voce perdeu!");
+        }else {
+            System.out.println("Voce esgotou suas tentativas e perdeu! ");
+            System.out.println(tentativas + " tentativas utilizadas ");
         }
-
     }
+}
